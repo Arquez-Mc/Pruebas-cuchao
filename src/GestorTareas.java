41809b9164f83eaf13885d95780b081f8d97e317
Arquez-Mc <163915164+Arquez-Mc@users.noreply.github.com>
2026-05-05 08:34:20 -0600

    feat: agregar nuevas cositas

--- src/GestorTareas.java
+++ src/GestorTareas.java
@@ -1,43 +1,106 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
-
+//*********************************************************************
+// CLASE GESTORTAREAS
 public class GestorTareas {
+    // LIST
     private List<String> tareas;
 
-//-------------------------------------
-//Constructor
+    // SET
+    private Set<String> tareasUnicas;
+
+    // QUEUE
+    private Queue<String> colaTareas;
+
+    // MAP
+    private Map<Integer, String> mapaTareas;
+
+    //-------------------------------------
+// Constructor
     public GestorTareas() {
         tareas = new ArrayList<>();
+        tareasUnicas = new HashSet<>();
+        colaTareas = new LinkedList<>();
+        mapaTareas = new HashMap<>();
     }
-//-------------------------------------
-    public void agregarTarea(String tarea) {
+
+    //*********************************************************************
+// PARTE LIST
+    public void agregarTareaLista(String tarea) {
         tareas.add(tarea);
     }
-//-------------------------------------
-    public void eliminarTarea(String tarea) {
+
+    public void eliminarTareaLista(String tarea) {
         tareas.remove(tarea);
     }
-//-------------------------------------
-    public void mostrarTareas() {
-        System.out.println("Lista de tareas:");
+
+    public void mostrarTareasLista() {
+        System.out.println("Lista de tareas (con duplicados):");
         for (String tarea : tareas) {
             System.out.println("- " + tarea);
         }
+    }
 
+    //*********************************************************************
+// PARTE SET
+    public void agregarTareaSet(String tarea) {
+        tareasUnicas.add(tarea);
+    }
 
+    public void eliminarTareaSet(String tarea) {
+        tareasUnicas.remove(tarea);
+    }
 
+    public void mostrarTareasSet() {
+        System.out.println("Tareas únicas (sin duplicados):");
+        for (String tarea : tareasUnicas) {
+            System.out.println("- " + tarea);
+        }
+    }
 
+    //*********************************************************************
+// PARTE QUEUE
+    public void agregarTareaQueue(String tarea) {
+        colaTareas.add(tarea);
+    }
 
+    public void procesarTareaQueue() {
+        String tarea = colaTareas.poll();
+        if (tarea != null) {
+            System.out.println("Procesando tarea: " + tarea);
+        } else {
+            System.out.println("No hay tareas en la cola.");
+        }
+    }
 
+    public void mostrarTareasQueue() {
+        System.out.println("Cola de tareas (FIFO):");
+        for (String tarea : colaTareas) {
+            System.out.println("- " + tarea);
+        }
+    }
 
+    //*********************************************************************
+// PARTE MAP
+    public void agregarTareaMap(int id, String tarea) {
+        mapaTareas.put(id, tarea);
+    }
 
+    public void eliminarTareaMap(int id) {
+        mapaTareas.remove(id);
+    }
 
-
-
-
-
-
-
+    public void mostrarTareasMap() {
+        System.out.println("Mapa de tareas (ID → Nombre):");
+        for (Map.Entry<Integer, String> entry : mapaTareas.entrySet()) {
+            System.out.println(entry.getKey() + " → " + entry.getValue());
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,53 @@ public class Main {
         // Creamos un gestor de tareas
         GestorTareas gestor = new GestorTareas();
 
-        // Agregamos tareas
-        gestor.agregarTarea("Estudiar colecciones");
-        gestor.agregarTarea("Hacer ejercicio");
-        gestor.agregarTarea("Leer un libro");
-        gestor.agregarTarea("Estudiar colecciones"); // duplicado permitido
+        //*********************************************************************
+        // LIST
+        gestor.agregarTareaLista("Estudiar colecciones");
+        gestor.agregarTareaLista("Hacer ejercicio");
+        gestor.agregarTareaLista("Leer un libro");
+        gestor.agregarTareaLista("Estudiar colecciones"); // duplicado permitido
 
-        // Mostramos todas las tareas
-        gestor.mostrarTareas();
+        gestor.mostrarTareasLista();
 
-        // Eliminamos una tarea
-        gestor.eliminarTarea("Hacer ejercicio");
+        gestor.eliminarTareaLista("Hacer ejercicio");
 
-        // Mostramos después de eliminar
-        gestor.mostrarTareas();
+        gestor.mostrarTareasLista();
 
+        //*********************************************************************
+        // SET
+        gestor.agregarTareaSet("Estudiar colecciones");
+        gestor.agregarTareaSet("Hacer ejercicio");
+        gestor.agregarTareaSet("Leer un libro");
+        gestor.agregarTareaSet("Estudiar colecciones"); // duplicado eliminado automáticamente
 
+        gestor.mostrarTareasSet();
 
+        gestor.eliminarTareaSet("Leer un libro");
 
+        gestor.mostrarTareasSet();
 
+        //*********************************************************************
+        // QUEUE
+        gestor.agregarTareaQueue("Tarea 1");
+        gestor.agregarTareaQueue("Tarea 2");
+        gestor.agregarTareaQueue("Tarea 3");
 
+        gestor.mostrarTareasQueue();
 
+        gestor.procesarTareaQueue(); // procesa la primera
+        gestor.mostrarTareasQueue();
+
+        //*********************************************************************
+        // MAP
+        gestor.agregarTareaMap(1, "Estudiar colecciones");
+        gestor.agregarTareaMap(2, "Hacer ejercicio");
+        gestor.agregarTareaMap(3, "Leer un libro");
+
+        gestor.mostrarTareasMap();
+
+        gestor.eliminarTareaMap(2);
+
+        gestor.mostrarTareasMap();
     }
 }
